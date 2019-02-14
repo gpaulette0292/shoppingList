84f99702311f15f92ab662939a03c283dd4b501f
Grace Paulette <gpaulette0292@jh-mba158-15.local>
2019-02-14 11:17:36 -0600

    added Item class

---
+++ Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  shoppingList
+//
+//  Created by Grace Paulette on 2/14/19.
+//  Copyright © 2019 John Hersey HIgh School. All rights reserved.
+//
+
+import Foundation
+class Item {
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+}
--- shoppingList/ViewController.swift
+++ shoppingList/ViewController.swift
@@ -8,13 +8,25 @@
 // Rebecca Rocks
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var newItemTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       tableView.dataSource = self
     }
 
-
+    @IBAction func addNewItemButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
 }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
+        cell.textLabel?.text = "Hello"
+        return cell
+}
+}
